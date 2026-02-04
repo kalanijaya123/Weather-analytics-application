453720a9c98cd445cd52252bd73e21906871b016
kalani <kalanidasunika@gmail.com>
2026-02-05 05:01:36 +0530

    chore: Update build artifacts

--- backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d205178dd304df70df64067003a7a4e575956e2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00a6893d1e68009d3fb34d0136d097d804f065c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
